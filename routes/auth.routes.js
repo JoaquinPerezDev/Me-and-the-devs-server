d7b6f6353d6b0672c6d62987f122578a17ec3315
LucasPerezDev <LucasPerezDev@gmail.com>
2022-05-14 18:13:09 -0400

    completed authentication creation process

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
-const { isAuthenticated } = require('./../middleware/jwt.middleware.js'); // <== IMPORT
+const { isAuthenticated } = require("./../middleware/jwt.middleware.js"); // <== IMPORT
 
 // How many rounds should bcrypt run the salt (default [10 - 12 rounds])
 const saltRounds = 10;
@@ -17,116 +17,105 @@ router.get("/loggedin", (req, res) => {
   res.json(req.user);
 });
 
-router.post('/signup', (req, res, next) => {
+router.post("/signup", (req, res, next) => {
   const { email, password, name } = req.body;
- 
 
-  if (email === '' || password === '' || name === '') {
+  if (email === "" || password === "" || name === "") {
     res.status(400).json({ message: "Provide email, password and name" });
     return;
   }
- 
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
   if (!emailRegex.test(email)) {
-    res.status(400).json({ message: 'Provide a valid email address.' });
+    res.status(400).json({ message: "Provide a valid email address." });
     return;
   }
-  
 
   const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
   if (!passwordRegex.test(password)) {
-    res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
+    res.status(400).json({
+      message:
+        "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+    });
     return;
   }
- 
- 
 
   User.findOne({ email })
     .then((foundUser) => {
-
       if (foundUser) {
         res.status(400).json({ message: "User already exists." });
         return;
       }
- 
 
       const salt = bcrypt.genSaltSync(saltRounds);
       const hashedPassword = bcrypt.hashSync(password, salt);
- 
- 
+
       return User.create({ email, password: hashedPassword, name });
     })
     .then((createdUser) => {
-
       const { email, name, _id } = createdUser;
-    
 
       const user = { email, name, _id };
- 
 
       res.status(201).json({ user: user });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.status(500).json({ message: "Internal Server Error" })
+      res.status(500).json({ message: "Internal Server Error" });
     });
 });
 
-router.post('/login', (req, res, next) => {
+router.post("/login", (req, res, next) => {
   const { email, password } = req.body;
- 
-  // Check if email or password are provided as empty string 
-  if (email === '' || password === '') {
+
+  // Check if email or password are provided as empty string
+  if (email === "" || password === "") {
     res.status(400).json({ message: "Provide email and password." });
     return;
   }
- 
+
   // Check the users collection if a user with the same email exists
   User.findOne({ email })
     .then((foundUser) => {
-    
       if (!foundUser) {
         // If the user is not found, send an error response
-        res.status(401).json({ message: "User not found." })
+        res.status(401).json({ message: "User not found." });
         return;
       }
- 
+
       // Compare the provided password with the one saved in the database
       const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
- 
+
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
         const { _id, email, name } = foundUser;
-        
+
         // Create an object that will be set as the token payload
         const payload = { _id, email, name };
- 
+
         // Create and sign the token
-        const authToken = jwt.sign( 
-          payload,
-          process.env.TOKEN_SECRET,
-          { algorithm: 'HS256', expiresIn: "6h" }
-        );
- 
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "6h",
+        });
+
         // Send the token as the response
         res.status(200).json({ authToken: authToken });
-      }
-      else {
+      } else {
         res.status(401).json({ message: "Unable to authenticate the user" });
       }
- 
     })
-    .catch(err => res.status(500).json({ message: "Internal Server Error" }));
+    .catch((err) => res.status(500).json({ message: "Internal Server Error" }));
 });
- 
+
 // GET  /auth/verify  -  Used to verify JWT stored on the client
-router.get('/verify', isAuthenticated, (req, res, next) => {       // <== CREATE NEW ROUTE
- 
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  // <== CREATE NEW ROUTE
+
   // If JWT token is valid the payload gets decoded by the
   // isAuthenticated middleware and made available on `req.payload`
   console.log(`req.payload`, req.payload);
- 
+
   // Send back the object with user data
   // previously set as the token payload
   res.status(200).json(req.payload);
