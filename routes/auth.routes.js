958b4a08d3cacb79dc51c6374e1ebd3b573270d4
LucasPerezDev <LucasPerezDev@gmail.com>
2022-05-17 11:19:14 -0400

    fixed some minor bugs involving username and email edit route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -82,19 +82,20 @@ router.post(
 
 router.post("/login", (req, res, next) => {
   const { email, password } = req.body;
-
+  console.log(req.body);
   // Check if email or password are provided as empty string
   if (email === "" || password === "") {
     res.status(400).json({ message: "Provide email and password." });
     return;
   }
-
+  console.log(email, password);
   // Check the users collection if a user with the same email exists
   User.findOne({ email })
     .then((foundUser) => {
+      console.log("user=", foundUser);
       if (!foundUser) {
         // If the user is not found, send an error response
-        res.status(401).json({ message: "User not found." });
+        res.status(403).json({ message: "User not found." });
         return;
       }
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -23,13 +23,25 @@ module.exports = router;
 
 router.put("/user/:userId/edit", (req, res, next) => {
   const { userId } = req.params;
+  const { name } = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(UserId)) {
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
 
+  if ( name.length <= 1) {
+    res.status(400).json({ message: "Please provide a real name" });
+    return;
+  }
+//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+//   if (!emailRegex.test(email)) {
+//     res.status(400).json({ message: "Provide a valid email address." });
+//     return;
+//   }
+
+
   User.findByIdAndUpdate(userId, req.body, { new: true })
-    .then((updatedUser) => res.json(updatedUser))
+    .then((updatedUser) => res.status(200).json(updatedUser))
     .catch((error) => res.json(error));
 });
